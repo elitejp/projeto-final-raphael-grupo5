6ed110fbfe2a99e7e168f3b6a81202017b1ee4ca
João Pedro Porto Teixeira <joao.porto.ismart@gmail.com>
2022-07-07 15:48:50 -0300

    fix: register, name para usar no formulario

--- src/components/Input/index.jsx
+++ src/components/Input/index.jsx
@@ -1,13 +1,13 @@
 import { StyledContainer, StyledInput } from "./styles";
 
-function Input({ label, isGray = false, icon: Icon, ...rest }) {
+function Input({ label, isGray = false, icon: Icon, register, name, ...rest }) {
   return (
     <StyledContainer>
       <div>{label}</div>
 
       <StyledInput isGray={isGray}>
         {Icon && <Icon />}
-        <input {...rest} />
+        <input {...register(name)} {...rest} />
       </StyledInput>
     </StyledContainer>
   );
