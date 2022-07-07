fa83e4ded2f9db39d37f29836505060c89f72051
lmFreire <l.matheus.freire10@gmail.com>
2022-07-07 08:08:23 -0300

    feat: page home

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,3 +1,18 @@
-function Header() {}
+import { StyledHeader } from "./styles";
+
+function Header() {
+  return (
+    <StyledHeader>
+      <h1>
+        i<span>Pet</span>
+      </h1>
+      <div>
+        <button>Sair</button>
+        <span>|</span>
+        <button>Dúvidas frequentes</button>
+      </div>
+    </StyledHeader>
+  );
+}
 
 export default Header;
--- src/components/Header/styles.js
+++ src/components/Header/styles.js
@@ -1,1 +1,32 @@
 import styled from "styled-components";
+
+export const StyledHeader = styled.header`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 15px 10%;
+  background-color: var(--grey100);
+
+  h1 {
+    color: var(--grey300);
+
+    span {
+      color: var(--secondary0);
+      margin-left: 1px;
+    }
+  }
+
+  div {
+    display: flex;
+    gap: 0.5rem;
+
+    button {
+      background: none;
+      border: none;
+
+      color: var(--grey300);
+
+      cursor: pointer;
+    }
+  }
+`;
