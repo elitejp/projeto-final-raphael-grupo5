3e3c9a4bb42bc3b2c51d7dd80a158d959b80c3d0
Heitor <heitor.cardoso39@gmail.com>
2022-07-08 09:37:32 -0300

    fix: correção de bug

--- src/components/Input/index.jsx
+++ src/components/Input/index.jsx
@@ -1,6 +1,5 @@
 import { StyledContainer, StyledInput } from "./styles";
 
-<<<<<<< HEAD
 function Input({ label, isGray = false, icon: Icon, register, name, error, ...rest }) {
 	return (
 		<StyledContainer>
@@ -11,17 +10,6 @@ function Input({ label, isGray = false, icon: Icon, register, name, error, ...re
 			<StyledInput isGray={isGray} isError={!!error}>
 				{Icon && <Icon />}
 				<input {...register(name)} {...rest} />
-=======
-function Input({ label, icon: Icon, ...rest }) {
-	console.log(rest);
-	return (
-		<StyledContainer>
-			<div>{label}</div>
-
-			<StyledInput>
-				{Icon && <Icon />}
-				<input {...rest} />
->>>>>>> 55aca01344e37b6e4ebbe9c25bd13eca616b0a6e
 			</StyledInput>
 		</StyledContainer>
 	);
