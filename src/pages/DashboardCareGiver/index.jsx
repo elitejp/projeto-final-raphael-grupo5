6b0d2a3d5e6e99e47c8bdb253c8ad5a2c4954d97
Heitor <heitor.cardoso39@gmail.com>
2022-07-09 01:41:28 -0300

    feat: criação do dashboard do cuidador

--- src/pages/DashboardCareGiver/index.jsx
+++ src/pages/DashboardCareGiver/index.jsx
@@ -1,3 +1,118 @@
-function DashboardCareGiver() {}
+import { useEffect, useState } from "react";
+import Header from "../../components/Header";
+import { apiCare } from "../../services";
+import ContainerDashboardCareGiver from "../../styles/styleDashboardCareGiver";
+import Button from "../../components/Button";
+import { toast } from "react-toastify";
+
+function DashboardCareGiver() {
+	const token = localStorage.getItem("Token");
+	const user = JSON.parse(localStorage.getItem("User"));
+	const [requests, setRequests] = useState([]);
+	const [showRequests, setShowRequests] = useState(false);
+
+	useEffect(() => {
+		apiCare
+			.get(`/users/${user.id}?_embed=request`, {
+				headers: {
+					Authorization: `Bearer ${token}`,
+				},
+			})
+			.then((response) => {
+				setRequests(response.data.request);
+				if (response.data.request.length === 0) {
+					setShowRequests(false);
+				} else {
+					setShowRequests(true);
+				}
+			})
+			.catch((err) => {
+				if (err.response.data === "jwt expired") {
+					toast.error("Seu login expirou! logue novamente.");
+				}
+			});
+	}, [user.id, token]);
+
+	return (
+		<>
+			<Header />
+			<ContainerDashboardCareGiver>
+				<div className="container">
+					<div className="infoCaregiver">
+						<h2>Cuidador</h2>
+						<div>
+							<p>{user.name}</p>
+							<img
+								src="https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460__340.png"
+								alt="perfil"
+							/>
+						</div>
+					</div>
+					<p>Donos interessados</p>
+					<ul className="infoOwners">
+						{showRequests ? (
+							requests.map((el) => (
+								<li>
+									<div>
+										<p>Nome do dono:</p>
+										<p>{el.name}</p>
+									</div>
+									<div>
+										<p>Número de pets:</p>
+										<p>{el.pet.length}</p>
+									</div>
+
+									<div>
+										<p>Dias necessários:</p>
+										<p>Não tem na API</p>
+									</div>
+									<div>
+										<p>Telefone de contato:</p>
+										<p>Não tem na API</p>
+									</div>
+									<ul className="infoPets">
+										{el.pet?.map((el) => (
+											<li>
+												<h3>Pet: {el.nome}</h3>
+												<div>
+													<p>Nome:</p>
+													<p>{el.nome}</p>
+												</div>
+												<div>
+													<p>Tipo de animal:</p>
+													<p>{el.tipo}</p>
+												</div>
+												<div>
+													<p>Idade:</p>
+													<p>{el.idade}</p>
+												</div>
+												<div>
+													<p>Porte físico:</p>
+													<p>{el.porte_físico}</p>
+												</div>
+												<div>
+													<p>Raça:</p>
+													<p>{el.raca}</p>
+												</div>
+												<div className="obsPet">
+													<p>Observações e cuidados:</p>
+													<p>{el.observações_cuidados}</p>
+												</div>
+											</li>
+										))}
+									</ul>
+									<Button>Aceitar</Button>
+									<Button>Recusar</Button>
+								</li>
+							))
+						) : (
+							<p>Você ainda não possui requisições!</p>
+						)}
+					</ul>
+				</div>
+			</ContainerDashboardCareGiver>
+		</>
+	);
+}
 
 export default DashboardCareGiver;
---
+++ src/styles/styleDashboardCareGiver.js
@@ -0,0 +1,91 @@
+import styled from "styled-components";
+
+const ContainerDashboardCareGiver = styled.div`
+	background-color: var(--grey300);
+	display: flex;
+	flex-direction: column;
+	justify-content: center;
+	padding: 10px;
+	font-family: "Inter", sans-serif;
+	font-weight: 600;
+	gap: 5px;
+
+	.infoCaregiver {
+		display: flex;
+		justify-content: space-between;
+		align-items: center;
+		width: 100%;
+
+		img {
+			width: 50px;
+			height: 50px;
+			border-radius: 100%;
+		}
+
+		div {
+			display: flex;
+			justify-content: center;
+			align-items: center;
+			gap: 15px;
+		}
+	}
+
+	.infoOwners {
+		border: 4px solid var(--secondary100);
+		border-radius: 4px;
+		display: flex;
+		padding: 10px;
+
+		li {
+			display: flex;
+			flex-direction: column;
+			gap: 10px;
+			width: 100%;
+		}
+
+		div {
+			display: flex;
+			justify-content: space-between;
+
+			p:nth-child(1) {
+				font-weight: 600;
+			}
+		}
+	}
+
+	.infoPets {
+		display: flex;
+		flex-direction: column;
+		gap: 10px;
+		font-weight: 400;
+
+		li {
+			background-color: var(--primary200);
+			border-radius: 5px;
+			padding: 7px;
+
+			div {
+				background-color: var(--primary300);
+				padding: 2px 5px 2px 5px;
+			}
+		}
+		.obsPet {
+			flex-direction: column;
+			gap: 5px;
+		}
+	}
+
+	@media (min-width: 600px) {
+		justify-content: center;
+		align-items: center;
+		.container {
+			max-width: 600px;
+			width: 100vw;
+
+			display: flex;
+			flex-direction: column;
+		}
+	}
+`;
+
+export default ContainerDashboardCareGiver;
